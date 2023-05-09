4e8c8b8733f83565f94f0d07478c65ae8f9510e3
ChristianRomo37 <CIRomo@student.fullsail.edu>
2023-05-08 22:51:13 -0400

    All scripts have been fully set up, some have lines that have been marked out so there is ease of use with your scenes

--- HungrySwanProject/Assets/Scripts/enemyAI.cs
+++ HungrySwanProject/Assets/Scripts/enemyAI.cs
@@ -1,18 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class enemyAI : MonoBehaviour
+public class enemyAI : MonoBehaviour, IDamage
 {
+    [Header("-----Components-----")]
+    [SerializeField] Renderer model;
+    //[SerializeField] NavMeshAgent agent;
+
+    [Header("-----Enemy Stats-----")]
+    [SerializeField] int HP;
+
+    //[Header("-----Enemy Weapon-----")]
+    //[Range(2, 300)][SerializeField] int shootDist;
+    //[Range(0.1f, 3f)][SerializeField] float shootRate;
+    //[Range(1, 10)][SerializeField] int shootDamage;
+    //[SerializeField] GameObject bullet;
+
+    //bool isShooting;
+    Color colorOrg;
     // Start is called before the first frame update
     void Start()
     {
-        
+        colorOrg = model.material.color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //agent.SetDestination(gameManager.instance.player.transform.position);
+
+        //if (!isShooting)
+        //{
+        //    StartCoroutine(shoot());
+        //}
+    }
+
+    //IEnumerator shoot()
+    //{
+    //    isShooting = true;
+
+    //    Instantiate(bullet, transform.position, transform.rotation);
+    //    yield return new WaitForSeconds(shootRate);
+
+    //    isShooting = false;
+    //}
+
+    public void takeDamage(int damage)
+    {
+        HP -= damage;
+        StartCoroutine(flashColor());
+
+        if (HP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator flashColor()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = colorOrg;
     }
 }
