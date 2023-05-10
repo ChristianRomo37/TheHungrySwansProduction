be8f5b2307ef9003729552261e3aebf2c163880f
Tyrano840 <61438260+Tyrano840@users.noreply.github.com>
2023-05-10 09:03:20 -0400

    uncommented the pause and unpause state.

--- HungrySwanProject/Assets/Scripts/gameManager.cs
+++ HungrySwanProject/Assets/Scripts/gameManager.cs
@@ -12,13 +12,13 @@ public class gameManager : MonoBehaviour
     //public GameObject playerSpawnPos;
 
     //[Header("-----UI Stuff-----")]
-    //public GameObject activeMenu;
-    //public GameObject pauseMenu;
+    public GameObject activeMenu;
+    public GameObject pauseMenu;
     //public GameObject loseMenu;
     //public GameObject winMenu;
 
-    //public bool isPaused;
-    //float timeScaleOrig;
+    public bool isPaused;
+    float timeScaleOrig;
     int enemiesRemaining;
 
     // Start is called before the first frame update
@@ -27,38 +27,38 @@ public class gameManager : MonoBehaviour
         instance = this;
         player = GameObject.FindGameObjectWithTag("Player");
         playerScript = player.GetComponent<playerControler>();
-        //timeScaleOrig = Time.timeScale;
+        timeScaleOrig = Time.timeScale;
         //playerSpawnPos = GameObject.FindGameObjectWithTag("Player Spawn Pos");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetButtonDown("Cancel") && activeMenu == null)
-        //{
-        //    isPaused = !isPaused;
-        //    activeMenu = pauseMenu;
-        //    activeMenu.SetActive(isPaused);
-        //    pauseState();
-        //}
+        if (Input.GetButtonDown("Cancel") && activeMenu == null)
+        {
+            isPaused = !isPaused;
+            activeMenu = pauseMenu;
+            activeMenu.SetActive(isPaused);
+            pauseState();
+        }
     }
 
-    //public void pauseState()
-    //{
-    //    Time.timeScale = 0;
-    //    Cursor.visible = true;
-    //    Cursor.lockState = CursorLockMode.Confined;
-    //}
+    public void pauseState()
+    {
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
 
-    //public void unPauseState()
-    //{
-    //    Time.timeScale = timeScaleOrig;
-    //    Cursor.lockState = CursorLockMode.Locked;
-    //    Cursor.visible = false;
-    //    isPaused = !isPaused;
-    //    activeMenu.SetActive(false);
-    //    activeMenu = null;
-    //}
+    public void unPauseState()
+    {
+        Time.timeScale = timeScaleOrig;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        isPaused = !isPaused;
+        activeMenu.SetActive(false);
+        activeMenu = null;
+    }
 
     //public void youLose()
     //{
