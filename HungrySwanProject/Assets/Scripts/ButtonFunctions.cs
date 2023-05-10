2a98edc487182d3534b0169b7c56171a52406e48
ChristianRomo37 <CIRomo@student.fullsail.edu>
2023-05-10 14:14:20 -0400

    Fixed the gun model and added the UI as prefab

--- HungrySwanProject/Assets/Scripts/ButtonFunctions.cs
+++ HungrySwanProject/Assets/Scripts/ButtonFunctions.cs
@@ -5,32 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class ButtonFunctions : MonoBehaviour
 {
-    //public void resume()
-    //{
-    //    gameManager.instance.unPauseState();
-    //    //gameManager.instance.isPaused = !gameManager.instance.isPaused;
-    //}
+    public void resume()
+    {
+        gameManager.instance.unPauseState();
+        //gameManager.instance.isPaused = !gameManager.instance.isPaused;
+    }
 
-    //public void restart()
-    //{
-    //    gameManager.instance.unPauseState();
-    //    // not good code if you decide to make a bigger game since it will take time to reload back to restart
-    //    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    //}
+    public void restart()
+    {
+        gameManager.instance.unPauseState();
+        // not good code if you decide to make a bigger game since it will take time to reload back to restart
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 
-    //public void Quit()
-    //{
-    //    Application.Quit();
-    //}
+    public void Quit()
+    {
+        Application.Quit();
+    }
 
     //public void respawnPlayer()
     //{
     //    gameManager.instance.unPauseState();
     //    gameManager.instance.playerScript.spawnPlayer();
-    //}
-
-    //public void playerHeal(int amount)
-    //{
-    //    gameManager.instance.playerScript.playerHeal(amount);
     //}
 }
