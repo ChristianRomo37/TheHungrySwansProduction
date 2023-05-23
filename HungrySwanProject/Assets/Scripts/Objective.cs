54858b0d1ea331d0e9a3f89943b8e0d4bbce6d72
ChristianRomo37 <CIRomo@student.fullsail.edu>
2023-05-23 13:45:12 -0400

    Revert "Merge branch 'main' of https://github.com/ChristianRomo37/TheHungrySwansProduction"
    This reverts commit 0c01e36c909767afa4d44af6ed02212a0037cabe, reversing
    changes made to 40bc4fe88389b915f89c38eb3e8856ee738f9dcc.

--- HungrySwanProject/Assets/Scripts/Objective.cs
+++ HungrySwanProject/Assets/Scripts/Objective.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TirePickup : MonoBehaviour
+public class Objective : MonoBehaviour, IInteractable
 {
     void Start()
     {
         gameManager.instance.updateGameGoal(1);
     }
 
-    public void interact(bool canInteract)
+    public void interact(bool canInteract) 
     {
         gameManager.instance.updateGameGoal(-1);
-        gameManager.instance.hasBattery = true;
         Destroy(gameObject);
     }
 
