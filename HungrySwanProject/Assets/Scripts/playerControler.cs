1ced9aab03011a6439cb60886e308c09137358db
ChristianRomo37 <CIRomo@student.fullsail.edu>
2023-05-23 13:45:52 -0400

    Revert "imported new muzzel flashes and now I need to add them in"
    This reverts commit 40bc4fe88389b915f89c38eb3e8856ee738f9dcc.

--- HungrySwanProject/Assets/Scripts/playerControler.cs
+++ HungrySwanProject/Assets/Scripts/playerControler.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class playerControler : MonoBehaviour, IDamage
 {
@@ -21,6 +20,7 @@ public class playerControler : MonoBehaviour, IDamage
 
     [Header("-----Weapon Stats-----")]
     public List<gunStats> gunList = new List<gunStats>();
+    [SerializeField] Transform shootPos;
     [Range(0, 300)][SerializeField] int shootDist;
     [Range(0, 3f)][SerializeField] float shootRate;
     [Range(1, 10)][SerializeField] int shootDamage;
@@ -31,12 +31,6 @@ public class playerControler : MonoBehaviour, IDamage
     [Range(0,500)][SerializeField] int bulletsRemaining;
     [SerializeField] MeshFilter gunModel;
     [SerializeField] MeshRenderer gunMat;
-    [SerializeField] GameObject rifleFlash;
-    [SerializeField] GameObject sniperFlash;
-    [SerializeField] GameObject pistolFlash;
-    public bool rifle;
-    public bool sniper;
-    public bool pistol;
 
     [Header("-----Audio-----")]
     [SerializeField] AudioClip[] audJump;
@@ -101,7 +95,7 @@ public class playerControler : MonoBehaviour, IDamage
 
                 if (gunList.Count > 0 && gunList[selectedGun].bulletsRemaining > 0 && !isReloading)
                 {
-                    //Debug.Log("shooting");
+                    Debug.Log("shooting");
                     StartCoroutine(shoot());
                 }
             }
@@ -195,9 +189,10 @@ public class playerControler : MonoBehaviour, IDamage
     {
         isShooting = true;
         gunList[selectedGun].bulletsRemaining -= gunList[selectedGun].shotsFired;
-                
+        
+        
         audioSource.PlayOneShot(gunList[selectedGun].gunShotAud, gunList[selectedGun].gunShotAudVol);
-        StartCoroutine(flashMuzzel());
+        Instantiate(gunList[selectedGun].muzzleFlash, gunList[selectedGun].shootPos.position, gunList[selectedGun].shootPos.transform.rotation);
 
         //bulletsShot++;
         gameManager.instance.updateBulletCounter();
@@ -217,36 +212,10 @@ public class playerControler : MonoBehaviour, IDamage
                 Instantiate(gunList[selectedGun].hitEffect, hit.point, gunList[selectedGun].hitEffect.transform.rotation);
             }
         }
-        
-
         //bulletsShot++;
         yield return new WaitForSeconds(shootRate);
 
         isShooting = false;
-    }
-
-    IEnumerator flashMuzzel()
-    {
-        Debug.Log("flash");
-        if (pistol == true)
-        {
-            pistolFlash.SetActive(true);
-        }
-        else if (rifle == true)
-        {
-            rifleFlash.SetActive(true);
-        }
-        else if (sniper == true)
-        {
-            sniperFlash.SetActive(true);
-        }
-
-        yield return new WaitForSeconds(.5f);
-
-        Debug.Log("noFlash");
-        pistolFlash.SetActive(false);
-        rifleFlash.SetActive(false);
-        sniperFlash.SetActive(false);
     }
 
     public void takeDamage(int amount)
@@ -325,9 +294,7 @@ public class playerControler : MonoBehaviour, IDamage
             gunList.Add(gunStat);
         }
 
-        sniper = gunStat.sniper;
-        pistol = gunStat.pistol;
-        rifle = gunStat.rifle;
+        shootPos = gunStat.shootPos;
         shootDamage = gunStat.shootDamage;
         shootDist = gunStat.shootDist;
         shootRate = gunStat.shootRate;
@@ -365,9 +332,7 @@ public class playerControler : MonoBehaviour, IDamage
 
     void changeGunStats()
     {
-        sniper = gunList[selectedGun].sniper;
-        pistol = gunList[selectedGun].pistol;
-        rifle = gunList[selectedGun].rifle;
+        shootPos = gunList[selectedGun].shootPos;
         shootDamage = gunList[selectedGun].shootDamage;
         shootDist = gunList[selectedGun].shootDist;
         shootRate = gunList[selectedGun].shootRate;
