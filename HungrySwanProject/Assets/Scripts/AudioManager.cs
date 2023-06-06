af9010ae1d86a2ed88ca3b52f562af36a0286628
Tyrano840 <96091000+TyrantofChaos@users.noreply.github.com>
2023-06-06 17:18:44 -0400

    Volume slider for the main menu

--- HungrySwanProject/Assets/Scripts/AudioManager.cs
+++ HungrySwanProject/Assets/Scripts/AudioManager.cs
@@ -1,8 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
-    
+    [SerializeField] string volumeParameter = "MasterVolume";
+    [SerializeField] AudioMixer mixer;
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] private float multiplier = 30f;
+
+    public void Awake()
+    {
+        volumeSlider.onValueChanged.AddListener(HandleSlider);
+    }
+
+    private void HandleSlider(float value)
+    {
+        mixer.SetFloat(volumeParameter, MathF.Log10(value) * multiplier);
+    }
 }
