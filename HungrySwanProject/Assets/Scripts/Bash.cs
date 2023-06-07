1905542a62c8cdd780583556028e0085193ef007
ChristianRomo37 <CIRomo@student.fullsail.edu>
2023-06-07 00:40:07 -0400

    fix for melee

--- HungrySwanProject/Assets/Scripts/Bash.cs
+++ HungrySwanProject/Assets/Scripts/Bash.cs
@@ -44,7 +44,6 @@ public class Bash : MonoBehaviour
         Rigidbody rb = other.GetComponent<Rigidbody>();
         IDamage dam = other.GetComponent<IDamage>();
 
-
         if (dam != null)
         {
             dam.takeDamage(dmg);
