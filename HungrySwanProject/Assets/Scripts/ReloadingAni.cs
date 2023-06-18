4b193dfccd368205c63e30ce6fa4abe60f853491
ChristianRomo37 <CIRomo@student.fullsail.edu>
2023-06-17 22:27:36 -0400

    reload animation bug fixed

--- HungrySwanProject/Assets/Scripts/ReloadingAni.cs
+++ HungrySwanProject/Assets/Scripts/ReloadingAni.cs
@@ -15,10 +15,21 @@ public class ReloadingAni : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (gameManager.instance.playerScript.gunList.Count > 0)
+        if (!gameManager.instance.playerScript.isReloading)
         {
+            anie.SetBool("Reloading", false);
+        }
+        else
+        {
+            anie.SetBool("Reloading", true);
+        }
+
+        if (gameManager.instance.playerScript.gunList.Count > 0 && !gameManager.instance.playerScript.isReloading)
+        {
+            CheckGun();
             if (!gameManager.instance.playerScript.magFull && gameManager.instance.playerScript.gunList[gameManager.instance.playerScript.selectedGun].totalBulletCount > 0)
             {
+                //CheckReload();
                 if (Input.GetButtonDown("Reload"))
                 {
                     //anie.SetBool("Reloading", true);
@@ -50,7 +61,7 @@ public class ReloadingAni : MonoBehaviour
                 }
                 else if (Input.GetButtonUp("Reload"))
                 {
-                    anie.SetBool("Reloading", false);
+                    //anie.SetBool("Reloading", false);
                     anie.SetBool("Rifle", false);
                     anie.SetBool("Pistol", false);
                     anie.SetBool("Shotgun", false);
@@ -59,5 +70,64 @@ public class ReloadingAni : MonoBehaviour
                 }
             }
         }
+
+
+    }
+
+    void CheckGun()
+    {
+        if (gameManager.instance.playerScript.pistol)
+        {
+            anie.SetBool("Pistol", true);
+        }
+        else
+            anie.SetBool("Pistol", false);
+
+        if (gameManager.instance.playerScript.sniper)
+        {
+            anie.SetBool("Sniper", true);
+        }
+        else
+            anie.SetBool("Sniper", false);
+
+        if (gameManager.instance.playerScript.rifle)
+        {
+            anie.SetBool("Rifle", true);
+        }
+        else
+            anie.SetBool("Rifle", false);
+
+        if (gameManager.instance.playerScript.uzi)
+        {
+            anie.SetBool("Uzi", true);
+        }
+        else
+            anie.SetBool("Uzi", false);
+
+        if (gameManager.instance.playerScript.shotgun)
+        {
+            anie.SetBool("Shotgun", true);
+        }
+        else
+        {
+            anie.SetBool("Shotgun", false);
+        }
+    }
+
+    void CheckReload()
+    {
+        if (Input.GetButton("Reload") == true)
+        {
+            anie.SetBool("Reloading", true);
+        }
+        else if (Input.GetButton("Reload") == false)
+        {
+            anie.SetBool("Reloading", false);
+        }
+    }
+
+    public void setFalseRe()
+    {
+        anie.SetBool("Reloading", false);
     }
 }
